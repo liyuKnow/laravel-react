f4be1d66177515cb87896390ccfc9fda8e2984c6
liyuKnow <kidus2011@gmail.com>
2022-12-18 14:52:47 -0800

    public, admin layouts and routes

--- public/frontend/src/routes/router.jsx
+++ public/frontend/src/routes/router.jsx
@@ -1,9 +1,39 @@
 import { createBrowserRouter } from "react-router-dom";
+import PublicLayout from "../components/layout/public/PublicLayout";
+import AdminLayout from "../components/layout/admin/AdminLayout";
 import Login from "../views/auth/Login";
 import Register from "../views/auth/Register";
 import Users from "../views/auth/Users";
+import NotFound from "../views/error/NotFound";
+import Home from "../views/public/Home";
+import Dashboard from "../views/admin/Dashboard";
+import AboutUs from "../views/public/AboutUs";
 
 const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <PublicLayout />,
+        children: [
+            {
+                path: "",
+                element: <Home />,
+            },
+            {
+                path: "/about",
+                element: <AboutUs />,
+            },
+        ],
+    },
+    {
+        path: "/admin",
+        element: <AdminLayout />,
+        children: [
+            {
+                path: "/admin",
+                element: <Dashboard />,
+            },
+        ],
+    },
     {
         path: "/login",
         element: <Login />,
@@ -12,10 +42,17 @@ const router = createBrowserRouter([
         path: "/register",
         element: <Register />,
     },
-    ,
     {
         path: "/users",
         element: <Users />,
+    },
+    {
+        path: "/users",
+        element: <Users />,
+    },
+    {
+        path: "*",
+        element: <NotFound />,
     },
 ]);
 
